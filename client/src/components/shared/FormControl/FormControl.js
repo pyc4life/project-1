09f94ab1373b9ea5c6066de681f258da038d5ff1
pyc4life <62721767+pyc4life@users.noreply.github.com>
2021-03-19 19:19:04 +0200

    create shared / reusable FormControl

---
+++ client/src/components/shared/FormControl/FormControl.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './FormControl.scss';
+
+const FormControl = ({
+    name,
+    placeholder,
+    label,
+    type,
+    onChangeHandler,
+    onBlurHandler,
+    errorMessage,
+}) => (
+    <div className="form-control">
+        <label
+            for={name}
+            className="form-control-label"
+        >
+            {label}
+        </label>
+        <input
+            className="form-control-input"
+            id={name}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            onChange={onChangeHandler}
+            onBlur={onBlurHandler}
+        />
+        <p className="form-control-warning">{errorMessage}</p>
+    </div>
+);
+
+export default FormControl;
