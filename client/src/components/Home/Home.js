fb9eaa4698f6c07477f1ee602805e296805cb97c
pyc4life <62721767+pyc4life@users.noreply.github.com>
2021-03-19 19:20:08 +0200

    create Home and NotFound components

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MainLayout from '../Layouts/MainLayout';
+
+import './Home.scss'
+
+const Home = () => {
+
+    return (
+        <MainLayout>
+            <h1>Home page</h1>
+        </MainLayout>
+    )
+}
+
+export default Home;
---
+++ client/src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+    <section>
+        <div>
+            <h1>404 | NOT FOUND</h1>
+        </div>
+    </section>
+);
+
+export default NotFound;
