68477cbd5dfbf24181d0fb11576b2c359351c9b5
pyc4life <62721767+pyc4life@users.noreply.github.com>
2021-03-21 16:23:17 +0200

    add method compare passwords and pre save hook

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
 
 const { Schema, model } = mongoose;
 
@@ -14,6 +17,36 @@ const userSchema = new Schema({
     }
 });
 
+userSchema.method('comparePasswords', (password) => {
+    return bcrypt.compare(password, this.password);
+});
+
+
+userSchema.pre('save', function (next) {
+
+    if (!this.isModified('password')) {
+        next();
+        return;
+    }
+
+    bcrypt.genSalt(saltRounds, (error, salt) => {
+        if (error) {
+            next(error);
+            return;
+        }
+
+        bcrypt.hash(this.password, salt, (error, hash) => {
+            if (error) {
+                next(error);
+                return;
+            }
+            this.password = hash;
+            next();
+        });
+    });
+})
+
 const User = new model('User', userSchema);
+
 
 export default User;
