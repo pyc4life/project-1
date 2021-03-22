a40c59dbc381a8654f7949e755688666d21d79d2
pyc4life <62721767+pyc4life@users.noreply.github.com>
2021-03-22 11:21:36 +0200

    create login Service

--- api/services/user-service.js
+++ api/services/user-service.js
@@ -4,6 +4,10 @@ const getUser = (username) => {
     return models.User.findOne({ username });
 };
 
+const getUsers = () => {
+    return models.User.find({});
+};
+
 const createUser = (userData) => {
     return getUser(userData.username)
         .then((user) => {
@@ -17,12 +21,33 @@ const createUser = (userData) => {
         });
 };
 
-const getUsers = () => {
-    return models.User.find({});
-}
+const login = (userData) => {
+
+    const { username, password } = userData;
+
+    return getUser(username)
+        .then(user => {
+            if (!user) {
+                throw {
+                    message: 'not register',
+                    code: 404,
+                };
+            }
+            return user.comparePasswords(password)
+        }).then(doPasswordsMatch => {
+            if (!doPasswordsMatch) {
+                throw {
+                    message: 'wrong password',
+                    code: 401,
+                };
+            }
+            return
+        })
+};
 
 export default {
     createUser,
     getUser,
     getUsers,
+    login,
 };
