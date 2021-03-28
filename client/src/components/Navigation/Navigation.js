2b5821c97591dc21014f616aad7a0284707a7ca0
pyc4life <62721767+pyc4life@users.noreply.github.com>
2021-03-28 16:16:11 +0300

    style navigation component

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Navigation.scss';
+
 const Navigation = () => (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/register">Register</Link>
+    <nav className="nav">
+        <ul className="nav-ul">
+            <li className="nav-list-item">
+                <Link className="nav-link" to="/">PROJECT-1 LOGO</Link>
             </li>
-            <li>
-                <Link to="/login">Login</Link>
+        </ul>
+        <ul className="nav-ul nav-right">
+            <li className="nav-list-item">
+                <Link className="nav-link" to="/register">Register</Link>
             </li>
-            <li>
-                <Link to="/">Hojme</Link>
+            <li className="nav-list-item">
+                <Link className="nav-link" to="/login">Login</Link>
             </li>
         </ul>
     </nav>
